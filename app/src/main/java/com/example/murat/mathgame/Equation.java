5eec55de6c5e1654f44c9ef6853a38e1308781b0
Mutra1 <42783914+Mutra1@users.noreply.github.com>
2018-10-14 18:21:59 -0500

    outline

---
+++ app/src/main/java/com/example/murat/mathgame/Equation.java
@@ -0,0 +1,5 @@
+package com.example.murat.mathgame;
+
+public class Equation {
+
+}
--- app/src/main/java/com/example/murat/mathgame/MainActivity.java
+++ app/src/main/java/com/example/murat/mathgame/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.murat.mathgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+    File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
