7cddc38eefd70e5b2e1c2b7aecf560f1c2179fce
Mutra1 <42783914+Mutra1@users.noreply.github.com>
2018-10-17 14:37:58 -0500

    UGHHH

--- app/src/main/java/com/example/murat/mathgame/MainActivity.java
+++ app/src/main/java/com/example/murat/mathgame/MainActivity.java
@@ -26,9 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         file = new File(getFilesDir(), "tokensandbadges.txt");
         game = new Game();
+        loadProgress();
     }
-
-
 
     //load function
     private void loadProgress() {
